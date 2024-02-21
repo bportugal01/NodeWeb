beda4e788f12a39f70b0e75ab4f46a1c8c58547e
bportugal01 <bruno2004sa@gmail.com>
2024-02-21 13:43:15 -0300

    Criando um servidor HTTP com JavaScript usando Node.js

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-var http = require("http")
+// Importa o módulo 'http' do Node.js
+var http = require("http");
 
+// Cria um servidor HTTP na porta 8081
 http.createServer(function (req, res) {
-    res.end("Hellou Mundou")
-}).listen(8081)
+    // Envia "Hellou Mundou" como resposta para qualquer requisição recebida
+    res.end("Hellou Mundou");
+}).listen(8081); // Faz o servidor escutar na porta 8081
 
-console.log("O servidor está rodando!")
+// Imprime mensagem no console informando que o servidor está rodando
+console.log("O servidor está rodando!");
